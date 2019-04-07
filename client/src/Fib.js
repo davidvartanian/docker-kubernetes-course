772b1cd1b40a0dd5fcd0b50e18b31f7018dfcbcf
David Vartanian <david.vartanian@mcmakler.de>
2019-04-08 00:29:38 +0200

    fix 'Hi' issue

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,6 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    if (!this.state.seenIndexes) {
-      this.state.seenIndexes = []
-    }
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
